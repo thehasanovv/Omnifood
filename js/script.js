0a4474266781e4f66557c2ac73b3eb437ac59e00
thehasanovv <hasanov.e95@gmail.com>
2024-03-06 21:08:29 +0400

    Implementing smooth scrolling

--- js/script.js
+++ js/script.js
@@ -2,7 +2,35 @@
 // Selectors
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
+const allLinks = document.querySelectorAll("a:link");
 
 ///////////////////////////////////////////////////////////
 // Make mobile navigation work
 btnNavEl.addEventListener("click", () => headerEl.classList.toggle("nav-open"));
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
